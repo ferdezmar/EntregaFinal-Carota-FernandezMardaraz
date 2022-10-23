b172f29ca15a6983f85d9259523f28470e202835
Federico Fernandez <fede.fernandezm91@gmail.com>
2022-10-23 16:41:50 -0300

    Terminé Registro e Inicio de Sesion del playground avanzado 2

--- avanzado/views.py
+++ avanzado/views.py
@@ -3,6 +3,8 @@ from avanzado.models import Mascota
 from avanzado.forms import MascotaFormulario
 from django.views.generic import ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required 
 
 def ver_mascotas (request):
     
@@ -10,7 +12,7 @@ def ver_mascotas (request):
     
     return render(request, 'avanzado/ver_mascotas.html', {'mascotas': mascotas})
 
-
+@login_required
 def crear_mascota (request):
     
     if request.method == 'POST':
@@ -80,13 +82,13 @@ class CrearMascota(CreateView):
     template_name = 'avanzado/crear_mascota_cbv.html'
     fields = ['nombre','tipo', 'edad', 'fecha_nacimiento']
     
-class EditarMascota(UpdateView):
+class EditarMascota(LoginRequiredMixin,UpdateView):
     model = Mascota
     success_url = '/avanzado/mascotas/'
     template_name = 'avanzado/editar_mascota_cbv.html'
     fields = ['nombre','tipo', 'edad', 'fecha_nacimiento']
     
-class EliminarMascota(DeleteView):
+class EliminarMascota(LoginRequiredMixin, DeleteView):
     model = Mascota
     success_url = '/avanzado/mascotas/'
     template_name = 'avanzado/eliminar_mascota_cbv.html'
