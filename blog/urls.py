b8d2bcb4a97347ff0b3276f5034523bdafdaf81f
Federico Carota <fedecarota.fc@gmail.com>
2022-10-31 17:23:04 -0300

    Se modificaron los path en el archivo urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from blog import views
 
 urlpatterns = [    
-    path('post/', views.ListaPost.as_view(), name='ver_post'),
-    path('post/crear/', views.CrearPost.as_view(), name='crear_post'),
-    path('post/editar/<int:pk>', views.EditarPost.as_view(), name='editar_post'),
-    path('post/eliminar/<int:pk>', views.EliminarPost.as_view(), name='eliminar_post'),
-    path('post/ver/<int:pk>', views.VerPost.as_view(), name='ver_post'),
+    path('pages/', views.ListaPost.as_view(), name='ver_post'),
+    path('pages/crear/', views.CrearPost.as_view(), name='crear_post'),
+    path('pages/editar/<int:pk>', views.EditarPost.as_view(), name='editar_post'),
+    path('pages/eliminar/<int:pk>', views.EliminarPost.as_view(), name='eliminar_post'),
+    path('pages/<int:pk>', views.VerPost.as_view(), name='ver_post'),
 ]
 
